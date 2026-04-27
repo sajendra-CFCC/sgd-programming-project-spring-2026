2d3e40c14b3393d238f7ed922c832be6d3ad1529
jdnemargut006 <jdnemargut006@810-134646>
2026-04-27 11:04:08 -0400

    player is vector world one

--- src/world1.cpp
+++ src/world1.cpp
@@ -10,28 +10,29 @@ namespace World1 {
     bool draw_spot;
     int text_x;
     int text_y;
-    int circ_x;
-    int circ_y;
     int boss_x;
     int boss_y;
     int boss_scale;
-    int rand_numx;
-    int rand_numy;
+    float rand_numx;
+    float rand_numy;
+    bool caught_spot;
+    Vector2 circ_pos;
 
     void Init() {
         world_complete = false;
         spawn_rand = false;
         draw_spot = true;
+        caught_spot = false;
         text_x = 145;
         text_y = 550;
-        circ_x = SCREEN_WIDTH/2;
-        circ_y = SCREEN_HEIGHT/2;
         boss_x = SCREEN_WIDTH / 2;
         boss_y = SCREEN_HEIGHT / 2 - 200;
         boss_scale = 1;
         std::srand(std::time(0));
         rand_numx = SCREEN_WIDTH / 2;
         rand_numy = SCREEN_HEIGHT / 2 + 200;
+        circ_pos.x = SCREEN_WIDTH / 2;
+        circ_pos.y = SCREEN_HEIGHT / 2;
     }
 
     WorldUpdateResult Update(GameState& game) {
@@ -56,20 +57,20 @@ namespace World1 {
         }
 
         if (IsKeyDown(KEY_LEFT)) {
-            circ_x--;
-            circ_x--;
+            circ_pos.x--;
+            circ_pos.x--;
         }
         if (IsKeyDown(KEY_RIGHT)) {
-            circ_x++;
-            circ_x++;
+            circ_pos.x++;
+            circ_pos.x++;
         }
         if (IsKeyDown(KEY_UP)) {
-            circ_y--;
-            circ_y--;
+            circ_pos.y--;
+            circ_pos.y--;
         }
         if (IsKeyDown(KEY_DOWN)) {
-            circ_y++;
-            circ_y++;
+            circ_pos.y++;
+            circ_pos.y++;
         }
 
         if (world_complete)
@@ -81,10 +82,9 @@ namespace World1 {
     void Draw(const GameState& game) {
                 
         DrawText("World 1 - Enter the shapes to damage the bosses", text_x, text_y, 20, WHITE);
-        DrawCircle(circ_x, circ_y, 10, BLUE);
+        DrawCircleV(circ_pos, 10, BLUE);
         if (draw_spot) {
-            DrawCircle(rand_numx, rand_numy, 10, RED);
-            draw_spot = false;
+            DrawCircle(rand_numx, rand_numy, 20, RED);
         }
         
         //get the current boss
