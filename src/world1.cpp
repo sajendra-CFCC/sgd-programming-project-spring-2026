5b3b77903437b5844fa1d6eabb6dfbc5faefa89b
jdnemargut006 <jdnemargut006@810-134646>
2026-04-27 11:19:51 -0400

    you can now damage the boss world one

--- src/world1.cpp
+++ src/world1.cpp
@@ -13,10 +13,9 @@ namespace World1 {
     int boss_x;
     int boss_y;
     int boss_scale;
-    float rand_numx;
-    float rand_numy;
     bool caught_spot;
     Vector2 circ_pos;
+    Vector2 rand_pos;
 
     void Init() {
         world_complete = false;
@@ -29,8 +28,8 @@ namespace World1 {
         boss_y = SCREEN_HEIGHT / 2 - 200;
         boss_scale = 1;
         std::srand(std::time(0));
-        rand_numx = SCREEN_WIDTH / 2;
-        rand_numy = SCREEN_HEIGHT / 2 + 200;
+        rand_pos.x = SCREEN_WIDTH / 2;
+        rand_pos.y = SCREEN_HEIGHT / 2 + 200;
         circ_pos.x = SCREEN_WIDTH / 2;
         circ_pos.y = SCREEN_HEIGHT / 2;
     }
@@ -40,8 +39,8 @@ namespace World1 {
 
         BossState& currentBoss = Bosses::ActiveBoss(game);
 
-        if (IsKeyPressed(KEY_E)) {
-            currentBoss.health -= 10;
+        if (spawn_rand) {
+            currentBoss.health -= 50;
         }
 
         if (currentBoss.health <= 0) {
@@ -51,24 +50,28 @@ namespace World1 {
         }
 
         if (spawn_rand) {
-            rand_numx = std::rand() % 801 + 0;
-            rand_numy = std::rand() % 601 + 0;
+            rand_pos.x = std::rand() % 801 + 0;
+            rand_pos.y = std::rand() % 601 + 0;
             spawn_rand = false;
         }
 
         if (IsKeyDown(KEY_LEFT)) {
             circ_pos.x--;
             circ_pos.x--;
+            circ_pos.x--;
         }
         if (IsKeyDown(KEY_RIGHT)) {
+            circ_pos.x++;
             circ_pos.x++;
             circ_pos.x++;
         }
         if (IsKeyDown(KEY_UP)) {
             circ_pos.y--;
             circ_pos.y--;
+            circ_pos.y--;
         }
         if (IsKeyDown(KEY_DOWN)) {
+            circ_pos.y++;
             circ_pos.y++;
             circ_pos.y++;
         }
@@ -84,9 +87,11 @@ namespace World1 {
         DrawText("World 1 - Enter the shapes to damage the bosses", text_x, text_y, 20, WHITE);
         DrawCircleV(circ_pos, 10, BLUE);
         if (draw_spot) {
-            DrawCircle(rand_numx, rand_numy, 20, RED);
+            DrawCircleV(rand_pos, 20, RED);
         }
-        
+        if (CheckCollisionCircles(circ_pos, 10, rand_pos, 20)) {
+            spawn_rand = true;
+        }
         //get the current boss
         const BossState& currentBoss = Bosses::ActiveBoss(game);
         int boss_size = 30 * boss_scale; //this is temporary and only works for first boss maybe
