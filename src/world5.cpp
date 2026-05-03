4e7c68aedb1fd599cfc39fb901ab565a6aa997bb
BrayTM <braybear999@gmail.com>
2026-05-03 02:18:15 -0400

    boss moves now yippee

--- src/world5.cpp
+++ src/world5.cpp
@@ -1,7 +1,8 @@
 #include "world5.h"
 #include "bosses.h"
 #include <iostream>
-// for whatever reason main.cpp has an error on the #include "raygui.h" on my system but it works fine anyway. Thank you VSCode, very cool!
+#include <cmath>
+
 namespace World5 {
     bool world_complete = false;
     bool Debug = false; // for testing purposes toggle with Q key
@@ -41,11 +42,12 @@ namespace World5 {
     }
     
     //what does each variable mean? use comments if not clear
-    void BossFireProj(int count, float delaypershot, int speed) { //be careful doing delay like this, it will hold up the whole program
+    // count is meant to be the number of projectiles fired, delaypershot is the delay between each projectile (technically in frames but its *60 so the input is in seconds), and speed is the speed of the projectiles.
+    void BossFireProj(int count, float delaypershot, int speed) { //Be careful doing delay like this, it will hold up the whole program
         for (int i = 0; i < count; i++) {
-            for (int d = 0; d < delaypershot * 60; d++) {
+            /*for (int d = 0; d < delaypershot * 60; d++) {
                 std::cout << "Waiting" << std::endl;
-            }
+            }*/
             std::cout << "Attempted to fire proj" << std::endl;
             // play shoot audio
         }
@@ -69,7 +71,8 @@ namespace World5 {
 
         MouseLoc = GetMousePosition();
 
-        Rectangle PlayerHitbox = { MouseLoc.x - (PlayerRad/2), MouseLoc.y - (PlayerRad/2), (PlayerRad), (PlayerRad) };
+        Rectangle PlayerHitbox = { MouseLoc.x - (PlayerRad/2), MouseLoc.y - (PlayerRad/2), (PlayerRad), (PlayerRad) }; //update hitboxes each frame
+        Rectangle BossHitbox = { BossPos.x - (BossRad/2), BossPos.y - (BossRad/2), (BossRad), (BossRad) };
 
         if (CheckCollisionRecs(PlayerHitbox, BossHitbox)) {
             game.health -= 1;
@@ -84,18 +87,28 @@ namespace World5 {
 
         //this bit of code is not clear, nor what you are trying to accomplish. Use comments if not clear
         //check random functions exposed by raylib, they can be easier to use
+        // this is meant to randomly attempt to fire projectiles each frame, with FireCD working as a cooldown between each trigger
         if (rand() % 3 == 0 && FireCD <= 0) {
             if (rand() % 2 == 0){
                 BossFireProj(rand() % 6 + 1, 1.0f, rand() % 3 + 1);
-            } else {
+            } else { // this bit is meant to either fire a larger or smaller number of projectiles
                 BossFireProj(rand() % 3 + 1, 1.0f, rand() % 3 + 1);
             }
             FireCD = rand() % (120 - 40 + 1) + 40;
+            BossAccel = 1.0f;
         } else {
             FireCD--;
         }
 
         BossState& currentBoss = Bosses::ActiveBoss(game);
+
+
+        BossAngletoPlayer = atan2(MouseLoc.y - BossPos.y, MouseLoc.x - BossPos.x); // gets the angle from the boss to the player to be used for moving the boss
+        BossPos.x += cos(BossAngletoPlayer) * BossAccel; // move the boss
+        BossPos.y += sin(BossAngletoPlayer) * BossAccel;
+
+
+        BossAccel += 0.04f; //boss speeds up over time
 
         if (world_complete) {
             return WORLD_COMPLETED;
@@ -114,7 +127,7 @@ namespace World5 {
         Bosses::Draw(currentBoss, BossPos.x, BossPos.y, 1);
         if (Debug == true) {
             DrawRectangleRec(PlayerHitbox, WHITE); // hitbox visualization
-            DrawRectangleRec(BossHitbox, WHITE);
+            DrawRectangle(BossPos.x - (BossRad/2), BossPos.y - (BossRad/2), (BossRad), (BossRad), WHITE); // changed cause old method didnt want to follow the boss after it started moving
             DrawText(TextFormat("Debug on"), MouseLoc.x - 49, MouseLoc.y + 25, 20, YELLOW);
             DrawLineV(MouseLoc, BossPos, GREEN); // line from player to boss
         }
