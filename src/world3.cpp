23c99a47391a7b65bcbc503224401ce19bb4658c
ewsmith464 <ewsmith464@mail.cfcc.edu>
2026-05-06 10:24:27 -0400

    Altered Damage amount from player to boss

--- src/world3.cpp
+++ src/world3.cpp
@@ -160,7 +160,7 @@ namespace World3 {
         //Collision for Boss and Ball
         if (CheckCollisionCircleRec(ballPosition, ballRadius, BossBox)) {
             std::cout << "HIT!\n";
-            currentBoss.health -= 10;
+            currentBoss.health -= 25;
             ballPosition = { 10, 10 };
             game.score += 150;
         }
